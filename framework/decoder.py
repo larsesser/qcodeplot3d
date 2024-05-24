815e8a2e24ae36dae8b5b23ef8a149687fc36904
Lars Esser <mail@lars-esser.de>
2024-05-24 14:48:36 +0200

    framework: split Decoder from Simulator into a separate class

---
+++ framework/decoder.py
@@ -0,0 +1,27 @@
+import abc
+from dataclasses import dataclass
+from framework.layer import Syndrome
+
+
+@dataclass
+class Decoder(abc.ABC):
+
+    @abc.abstractmethod
+    def decode(self, syndrome: Syndrome) -> list[int]:
+        """Determine the qubits on which a correction shall be applied from a given syndrome."""
+
+
+# Mapping of syndromes (detector events) to qubits of the correction.
+LookupTable = dict[tuple[bool, ...], list[int]]
+
+
+@dataclass
+class LookupTableDecoder(Decoder):
+    lookup_table: LookupTable
+
+    def decode(self, syndrome: Syndrome) -> list[int]:
+        """Determine the qubits on which a correction shall be applied from a given syndrome.
+
+        Use a (non-scalable) lookup table for decoding.
+        """
+        return self.lookup_table.get(syndrome.value, [])
