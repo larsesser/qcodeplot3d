efd2fa4818a95f3e7f80a489d9be458aaf741d43
Lars Esser <mail@lars-esser.de>
2024-07-18 14:00:50 +0200

    add simulation of [[15,1,3]] tetrahedron with lookup table
    z stabilizers are not working yet though

--- framework/decoder.py
+++ framework/decoder.py
@@ -1,5 +1,5 @@
 import abc
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from framework.syndrome import Syndrome
 from framework.util import Kind
@@ -21,10 +21,13 @@ LookupTable = dict[tuple[bool, ...], list[int]]
 @dataclass
 class LookupTableDecoder(Decoder):
     lookup_table: LookupTable
+    missed_syndromes: int = field(default=0, init=False)
 
     def decode(self, syndrome: Syndrome) -> list[int]:
         """Determine the qubits on which a correction shall be applied from a given syndrome.
 
         Use a (non-scalable) lookup table for decoding.
         """
+        if syndrome.value not in self.lookup_table:
+            self.missed_syndromes += 1
         return self.lookup_table.get(syndrome.value, [])
