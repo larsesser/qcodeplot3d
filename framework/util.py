7324fd71fd8c6f71c9a32719dc8a8ab6ec395459
Lars Esser <mail@lars-esser.de>
2024-07-18 10:56:37 +0200

    framework/layer: fix implementation of InitializeInstruction

--- framework/util.py
+++ framework/util.py
@@ -5,3 +5,11 @@ class Kind(enum.Enum):
     """Which kind of stabilizer?"""
     x = "x"
     z = "z"
+
+    @property
+    def opposite(self) -> "Kind":
+        if self == Kind.x:
+            return Kind.z
+        if self == Kind.z:
+            return Kind.x
+        raise NotImplementedError
