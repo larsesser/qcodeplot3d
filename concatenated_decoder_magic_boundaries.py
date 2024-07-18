fc9244cbfbb61fcc1d52502f637d332e291e583f
Lars Esser <mail@lars-esser.de>
2024-07-18 18:12:48 +0200

    remove missing import

--- concatenated_decoder_magic_boundaries.py
+++ concatenated_decoder_magic_boundaries.py
@@ -13,7 +13,6 @@ from framework.cc_3d.construction import (
 )
 from framework.cc_3d.decoder import ConcatenatedDecoder
 from framework.cc_3d.plotter import Plotter3D
-from framework.cc_3d.simulator import deterministic_simulation
 from framework.stabilizers import (
     Color,
     Operator,
